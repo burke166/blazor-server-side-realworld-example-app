11342a89bb8ca5a29bb03aad4ced4f61d5e0fce0
Chris Burke <chris@computercodeblue.com>
2019-04-14 11:16:39 -0500

    Merged with torhovland/blazor-realworld-example-app

---
+++ src/RazorComponentsRealworld/Model/ArticleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BlazorRealworld.Model
+{
+    public class ArticleModel
+    {
+        public ArticleModel()
+        {
+            author = new AuthorModel();
+        }
+
+        public string title { get; set; }
+        public string slug { get; set; }
+        public string body { get; set; }
+        public DateTime createdAt { get; set; }
+        public string[] tagList { get; set; }
+        public string description { get; set; }
+        public AuthorModel author { get; set; }
+        public int favoritesCount { get; set; }
+    }
+
+    public class AuthorModel
+    {
+        public string username { get; set; }
+        public string image { get; set; }
+    }
+}
---
+++ src/RazorComponentsRealworld/Model/ErrorsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorRealworld
+{
+    public class ErrorsModel
+    {
+        public string[] username { get; set; }
+        public string[] email { get; set; }
+        public string[] password { get; set; }
+        public string[] title { get; set; }
+        public string[] body { get; set; }
+        public string[] description { get; set; }
+        public string[] emailOrPassword { get; set; }
+    }
+}
